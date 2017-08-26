1888af7a0ee5304a55568586045305598cc430c7
Francisco Carrera <franciscocarrer@gmail.com>
2017-08-25 22:32:59 -0400

    Added pseudo code

---
+++ assets/js/brawl.js
@@ -0,0 +1,32 @@
+//
+// Define Variables
+//
+
+
+// Assign event functionality to elements
+
+// First State
+// Click any starting character
+// Character clicked goes left becomes player character
+// Other characters go right become enemies
+// New click functionality added for second phase
+
+// Second Stage
+// Clicking on an enemy brings him to the middle
+// Sets attack and counter attack values
+// Buttons to attack appear
+
+// Third Stage
+// Clicking Attack Button Damages player and enemy
+// Set new damage and health totals
+    // On Enemy kill
+        // Hide enemy 
+        // If there are more enemies repeat stage 2
+        // Else Game Over Player Wins
+    //On player kill
+        // Game Over Player Losses
+
+// Fourth Stage Reset Game
+    // Attack and Health Values to Original
+    // All Characters In middle
+    // Click buttons to first stage
